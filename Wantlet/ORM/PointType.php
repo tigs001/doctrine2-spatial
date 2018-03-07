febab49fc832536703933274e869ef5446fc0568
S Ferguson <sferguson@sfdell-u.syd.tigint.com>
2018-03-07 18:04:48 +1100

    Updated to work with ZendFramework version 2.4 and MySQL 5.7

--- Wantlet/ORM/PointType.php
+++ Wantlet/ORM/PointType.php
@@ -3,9 +3,13 @@ namespace Wantlet\ORM;
 
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\MySQL57Platform;
 
 /**
  * Mapping type for spatial POINT objects
+ *
+ * Some of these functions are from:
+ * http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/cookbook/advanced-field-value-conversion-using-custom-mapping-types.html#the-mapping-type
  */
 class PointType extends Type {
     const POINT = 'point';
@@ -35,13 +39,70 @@ class PointType extends Type {
             return null;
         }
 
+        if ($platform instanceof MySQL57Platform)
+        {
+        	list($longitude, $latitude) = sscanf($value, 'POINT(%f %f)');
+
+        	return new \Wantlet\ORM\Point($latitude, $longitude);
+        }
+
         $data = unpack('x/x/x/x/corder/Ltype/dlat/dlon', $value);
         return new \Wantlet\ORM\Point($data['lat'], $data['lon']);
     }
 
     public function convertToDatabaseValue($value, AbstractPlatform $platform) {
         if (!$value) return;
-        
+
+        if ($platform instanceof MySQL57Platform)
+        	return sprintf("POINT(%F %F)", $value->getLongitude(), $value->getLatitude());
+
         return pack('xxxxcLdd', '0', 1, $value->getLatitude(), $value->getLongitude());
+    }
+
+
+
+    /**
+     * Does working with this column require SQL conversion functions?
+     *
+     * This is a metadata function that is required for example in the ORM.
+     * Usage of {@link convertToDatabaseValueSQL} and
+     * {@link convertToPHPValueSQL} works for any type and mostly
+     * does nothing. This method can additionally be used for optimization purposes.
+     *
+     * @return boolean
+     */
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
+
+
+
+    /**
+     * Modifies the SQL expression (identifier, parameter) to convert to a PHP value.
+     *
+     * @param string                                    $sqlExpr
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function convertToPHPValueSQL($sqlExpr, $platform)
+    {
+        return sprintf('AsText(%s)', $sqlExpr);
+    }
+
+
+
+    /**
+     * Modifies the SQL expression (identifier, parameter) to convert to a database value.
+     *
+     * @param string                                    $sqlExpr
+     * @param \Doctrine\DBAL\Platforms\AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('PointFromText(%s)', $sqlExpr);
     }
 }
